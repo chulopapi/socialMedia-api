713c70a687c0dc27deb44cc9d2a489bef18347fd
chulopapi <marlon.guandique@gmail.com>
2020-10-03 19:34:33 -0700

    setting up server.js with mongoose, express setup and mongodb

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
+//Written by Marlon Guandique
+
 const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+//routes
+const routes = require('./routes')
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialMedia', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
