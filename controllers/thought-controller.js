be20860c950a4a0dd5d47841f1981fae01ec220a
chulopapi <marlon.guandique@gmail.com>
2020-10-04 16:59:47 -0700

    controllers thought and users JS files, initial configuration

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,18 @@
+// Thought controllers
+
 const { User, Thought } = require('../models');
+
+const thoughtController = {
+    async getAllThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find({})
+            .select('-__v')
+            res.json(thoughts)
+        }
+        catch(err) {
+            res.status(400).json(err)
+        }
+    }
+}
+
+module.exports = thoughtController
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,15 @@
 const { User, Thought } = require('../models');
+
+const userController = {
+    async getAllUsers(req,res) {
+        try {
+            const users = await User.find({})
+            .select('-__v')
+            res.json(users)
+        }
+        catch(err) {
+            res.status(400).json(err)
+        }
+    }
+}
+module.exports = userController
